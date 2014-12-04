ae250a2521d9f969dfd10a5f75f8a5d9cba2bb87
Matthew <matt@mattmc.info>
2014-12-04 19:11:14 +0100

    Done some basic backend stuff

--- src/main/java/com/dalthow/launcher/Base.java
+++ src/main/java/com/dalthow/launcher/Base.java
@@ -4,6 +4,38 @@ public class Base
 {
 	public static void main(String[] args)
 	{
-
+		// if(System.isMac){
+		// NOP
+		// SELFDESTRUCT();
+		// }
+	}
+	
+	private boolean updateAvalable(){
+		return false;
+	}
+	
+	private void downloadGame(){
+		
+	}
+	
+	private void downloadUpdate(boolean requireReset){
+		
+	}
+	
+	private boolean isDeveloper(){
+		return false;
+	}
+	
+	private boolean databaseRunning(){
+		return true;
+	}
+	
+	//returns true if login data is correct
+	private boolean isValidated(){
+		return false;
+	}
+	
+	private void launchGame(String username, String password){
+		
 	}
 }
